64c88f15ed052d16a559004f843f973cab347bfd
unknown <Administrator@DellVostro14.SYSNET.COM>
2025-04-08 17:47:12 +0600

    add to update sql file

--- sql_commands .sql
+++ sql_commands .sql
@@ -21,34 +21,37 @@ select id,name from emp;
 
 select distinct city from emp;
 
+//// salary is greater than or equal to 20000
 select *
 from emp 
 where salary>=20000;
 
-select *from emp order by salary desc;
 
+select *from emp order by salary desc;
+//// city is dhaka or salary is 20000
 select *from emp where city='dhaka' and salary=20000;
 
 select *from emp where city in ('dhaka','aaa');
-
+// between salary 15000 and 20000
 select *from emp where salary between 15000 and 20000;
 
 select *from emp;
-
+// name starts with 'd'
 select *from emp where city like 'd%';
-
+// name starts with 'n'
 select *from emp where name like '%n';
-
+// name ends with 'n'
 select *from emp where name like '_r%'
-
+// maximum salary
 select max(salary) as result from emp;
-
+//minmum salary
 select min(salary) as result from emp;
-
+// total sum of salary
 select sum (salary)from emp;
 
 select *from emp;
 
+// 2nd highest salary
 select max(salary) from emp where salary <(select max(salary) from emp);
-
+// 2nd lowest salary
 select min(salary)from emp where salary >(select min(salary)from emp);
